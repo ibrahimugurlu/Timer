5fb335c4612b43342e7b0a8c2472f3cd8f54dd0e
Ibrahim Ugurlu <ibrahimugurlu18@outlook.com>
2022-03-12 11:06:56 +0300

    Add project files.

---
+++ Timer/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿
+namespace Timer
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnPause = new System.Windows.Forms.Button();
+            this.btnYenile = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe Script", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(305, 92);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(167, 183);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(92, 33);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Başla";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnPause
+            // 
+            this.btnPause.Location = new System.Drawing.Point(305, 183);
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(92, 33);
+            this.btnPause.TabIndex = 2;
+            this.btnPause.Text = "Durdur";
+            this.btnPause.UseVisualStyleBackColor = true;
+            this.btnPause.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnYenile
+            // 
+            this.btnYenile.Location = new System.Drawing.Point(446, 183);
+            this.btnYenile.Name = "btnYenile";
+            this.btnYenile.Size = new System.Drawing.Size(92, 33);
+            this.btnYenile.TabIndex = 3;
+            this.btnYenile.Text = "Sıfırla";
+            this.btnYenile.UseVisualStyleBackColor = true;
+            this.btnYenile.Click += new System.EventHandler(this.btnYenile_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.progressBar1.Location = new System.Drawing.Point(167, 247);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(371, 23);
+            this.progressBar1.TabIndex = 4;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.btnYenile);
+            this.Controls.Add(this.btnPause);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnPause;
+        private System.Windows.Forms.Button btnYenile;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Timer/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Timer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int sayac = 0;
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            sayac++;
+            label1.Text = sayac.ToString();
+            progressBar1.Value = sayac;
+            switch(sayac)
+            {
+                case 10: this.BackColor = Color.Red; break;
+                case 25: this.BackColor = Color.Orange; break;
+                case 40: this.BackColor = Color.Yellow; break;
+                case 55: this.BackColor = Color.Green; break;
+                case 70: this.BackColor = Color.Blue; break;
+                case 85: this.BackColor = Color.Purple; break;
+            
+            }
+            if (sayac == 100)
+            {
+                sayac = 0;
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void btnYenile_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            sayac = 0;
+            progressBar1.Value = 0;
+            this.BackColor = Color.White;
+        }
+    }
+}
